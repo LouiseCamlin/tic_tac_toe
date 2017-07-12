f8cae38436a77466116253fe0aacf56c8df7a25c
Matt Rose <yamiacat@gmail.com>
2017-07-12 11:27:27 +0100

    Add static version of Controller, GameStatus and GameBoard

---
+++ client/src/components/GameStatus.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import GameBoard from './GameBoard.jsx'
+
+class GameStatus extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <GameBoard />
+      </div>
+    );
+  }
+
+}
+
+
+export default GameStatus;
---
+++ client/src/containers/Controller.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import GameStatus from '../components/GameStatus.jsx';
+
+
+class Controller extends React.Component {
+
+  render(){
+    return (
+      <div>
+        <GameStatus />
+      </div>
+    );
+  }
+
+
+}
+
+
+
+export default Controller;
